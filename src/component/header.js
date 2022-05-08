cf1def9963c542155c1f367a743f180a85e0e02a
Jos33y <josephlagbalu@gmail.com>
2022-05-08 02:44:43 +0100

    already made some changes

--- src/component/header.js
+++ src/component/header.js
@@ -12,12 +12,12 @@ function Header() {
             <header id="header" className="d-flex flex-column justify-content-center">
 
                     <Nav className="flex-column nav-menu">
-                            <NavLink href="/#hero" className="item"><i className='bx bx-home'></i><span className="text">Home</span></NavLink>
-                            <NavLink href="/#about" className="item"><i className='bx bx-user'></i><span className="text">About</span></NavLink>
-                            <NavLink href="/#resume" className="item"><i className='bx bx-file-blank'></i><span className="text">Resume</span></NavLink>
-                            <NavLink href="#" className="item"><i className='bx bx-book-content'></i><span className="text">Portfolio</span></NavLink>
-                            <NavLink href="/#services" className="item"><i className='bx bx-server'></i><span className="text">Services</span></NavLink>
-                            <NavLink href="/#contact" className="item"><i className='bx bx-envelope'></i><span className="text">Contact</span></NavLink>
+                            <NavLink href="#hero" className="item active"><i className='bx bx-home'></i><span className="text">Home</span></NavLink>
+                            <NavLink href="#about" className="item"><i className='bx bx-user'></i><span className="text">About</span></NavLink>
+                            <NavLink href="#resume" className="item"><i className='bx bx-file-blank'></i><span className="text">Resume</span></NavLink>
+                            <NavLink href="#about" className="item"><i className='bx bx-book-content'></i><span className="text">Portfolio</span></NavLink>
+                            <NavLink href="#services" className="item"><i className='bx bx-server'></i><span className="text">Services</span></NavLink>
+                            <NavLink href="#contact" className="item"><i className='bx bx-envelope'></i><span className="text">Contact</span></NavLink>
                     </Nav>
             </header>
 
